1ea2e910290f188a1c99f342a399341b7ac56316
ordinaryask <ordinaryask@gmail.com>
2023-02-15 19:09:12 +0100

    yarn create

---
+++ bin/cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process')
+
+const runCmd = cmd => {
+  try {
+    execSync(`${cmd}`, { stdio: 'inherit' })
+  } catch (e) {
+    console.error(`Failed to execute ${cmd}`, e)
+
+    return false
+  }
+
+  return true
+}
+
+const repoName = process.argv[2]
+const gitCheckoutCmd = `git clone --depth 1 https://github.com/BeerCodeIndustry/react-starter ${repoName}`
+const installDepsCmd = `cd ${repoName} && yarn install`
+
+console.log(`Cloning the repository with name ${repoName}`)
+const checkoutOut = runCmd(gitCheckoutCmd)
+if (!checkoutOut) process.exit(-1)
+
+console.log(`Installing dependencies for ${repoName}`)
+const installedDeps = runCmd(installDepsCmd)
+if (!installedDeps) process.exit(-1)
+
+console.log(
+  'Congratulations! You are ready. Follow the following commands to start',
+)
+console.log(`cd ${repoName} && yarn start`)
