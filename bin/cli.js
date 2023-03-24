53b71d34b1a2393b532b64f610bf256265fa2fef
ordinaryask <ordinaryask@gmail.com>
2023-03-24 15:34:38 +0100

    fix: remote shallow

--- bin/cli.js
+++ bin/cli.js
@@ -17,6 +17,7 @@ const runCmd = cmd => {
 const repoName = process.argv[2] ?? 'react-app'
 const gitCheckoutCmd = `git clone --depth 1 https://github.com/BeerCodeIndustry/react-starter ${repoName}`
 const installDepsCmd = `cd ${repoName} && yarn install`
+const removeRemoteCmd = `cd ${repoName} && git remote remove origin && git filter-branch -- --all && git update-ref -d HEAD`
 
 console.log(`Cloning the repository with name ${repoName}`)
 const checkoutOut = runCmd(gitCheckoutCmd)
@@ -25,6 +26,10 @@ if (!checkoutOut) process.exit(-1)
 console.log(`Installing dependencies for ${repoName}`)
 const installedDeps = runCmd(installDepsCmd)
 if (!installedDeps) process.exit(-1)
+
+console.log(`Remove remote`)
+const removedRemote = runCmd(removeRemoteCmd)
+if (!removedRemote) process.exit(-1)
 
 console.log(
   'Congratulations! You are ready. Follow the following commands to start',
